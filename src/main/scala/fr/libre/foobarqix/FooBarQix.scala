18606fa0a649552e82460c1cdd7a7206f2d74f58
ubourdon <bourdon.ugo@gmail.com>
2011-11-24 19:13:47 +0100

    refactor with suppress check boolean param

--- src/main/scala/fr/libre/foobarqix/FooBarQix.scala
+++ src/main/scala/fr/libre/foobarqix/FooBarQix.scala
@@ -13,29 +13,14 @@ class FooBarQix(utils: FooBarQixUtils = new FooBarQixUtils) {
 
     def run(input: Int): String = {
         val responseBuilder: StringBuilder = new StringBuilder
-        var check: Boolean = false
 
-        if( input % 3 == 0 ) {
-            responseBuilder.append(Foo)
-            check = true
-        }
+        if( input % 3 == 0 ) responseBuilder.append(Foo)
+        if( input % 5 == 0 ) responseBuilder.append(Bar)
+        if( input % 7 == 0 ) responseBuilder.append(Qix)
 
-        if( input % 5 == 0 ) {
-            responseBuilder.append(Bar)
-            check = true
-        }
+        responseBuilder append utils.appendConsideringApparitionDigit( utils.splitIntOnDigit( input ) )
 
-        if( input % 7 == 0 ) {
-            responseBuilder.append(Qix)
-            check = true
-        }
-
-        val result: String = utils.appendConsideringApparitionDigit( utils.splitIntOnDigit( input ) )
-        if(!result.isEmpty) check = true
-
-        responseBuilder append result
-
-        if( !check ) responseBuilder.append(input.toString)
+        if( responseBuilder.isEmpty ) responseBuilder.append(input.toString)
 
         responseBuilder.toString()
     }
